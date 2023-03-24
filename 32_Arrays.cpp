b7a5812776ed7164307b10aa39d8c0898307bd67
Domm29 <q.continuum19@gmail.com>
2023-03-24 12:27:02 +0000

    creating lists

--- 32_Arrays.cpp
+++ 32_Arrays.cpp
@@ -3,6 +3,27 @@
 
 int main() {
 
+    // array = data structure that can hold multiple values 
+        // add a square and the {};
+    // array must have only the same data type
+
+
+    std::string car[] = {"Covette", "Mustang", "Viper"};
+
+    car[0] = "Bugatti";
+    
+
+    // can create an empty array and set the size of the arrary 
+    int age[5];
+
+    age[0] = 35;
+    age[4] = 23;
+
+
+    std::cout << car[0];
+    std::cout << "\nYour age is: " << age[4]; 
+
+
 
 
 
