c6565b518ebb92a65424c50c448b7a46f96597c9
Domm29 <q.continuum19@gmail.com>
2023-03-24 17:51:24 +0000

    2 dimensional arrays

--- 41_MultidimensionalArrays.cpp
+++ 41_MultidimensionalArrays.cpp
@@ -4,8 +4,27 @@
 int main() {
 
 
+    // Multidimensioal array = 2D arrays
+
+    std::string films[3][3] = {{"Assasin Creed", "Call of Duty", "Game of Thrones"},
+                              {"Batman", "Shazam", "Superman"},
+                              {"Thor", "Iron Man", "Watchmen" }};
 
 
+    std::cout << films[0][1] << " ";
+
+
+    // displaying full array 
+    int rows = sizeof(films)/sizeof(films[0]);
+    int columns = sizeof(films[0])/sizeof(films[0][0]);
+
+    for(int i=0; i < rows; i++){
+        std::cout << '\n';
+        for(int j=0; j<columns; j++){
+            std::cout << films[i][j] << " ";
+
+        }
+    }
 
 
 
