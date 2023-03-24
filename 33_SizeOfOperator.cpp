49a16f69199befc08037192287af75c599d09c9c
Domm29 <q.continuum19@gmail.com>
2023-03-24 16:53:45 +0000

    updated documents

--- 33_SizeOfOperator.cpp
+++ 33_SizeOfOperator.cpp
@@ -4,10 +4,20 @@
 int main() {
 
 
+    // sizeof() = obtain the bytes of a variable or class
 
 
+    double restruant = 39990;
+    std::string name = "John Macintosh";
 
+    std::string flowers[] = {"Daffodil", "Rose"};
 
+    std::cout << sizeof(restruant) << " bytes\n";
+
+    std::cout << sizeof(name) << " bytes\n";
+
+    // can count the number of elements in our list
+    std::cout << sizeof(flowers)/sizeof(std::string) << " number of elements\n ";
 
 
     return 0;
--- 34_IterateOverAnArray.cpp
+++ 34_IterateOverAnArray.cpp
@@ -5,6 +5,16 @@ int main() {
 
 
 
+    // Iterating over the elements of an array 
+
+    std::string employees[] = {"Jose", "Micheal", "Jake"};
+
+    for(int i = 0; i < sizeof(employees)/sizeof(std::string); i++){
+
+        std::cout << employees[i] <<"\n";
+    }
+
+
 
 
 
