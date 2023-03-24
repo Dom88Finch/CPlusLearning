6f36c798a6bc95f043f95b9462f9dac0dff87552
Domm29 <q.continuum19@gmail.com>
2023-03-24 17:29:25 +0000

    using fill function

--- 39_FillFunction.cpp
+++ 39_FillFunction.cpp
@@ -5,10 +5,13 @@ int main() {
 
     // Fill function fill() - fills a range of elements with a specified value
 
-    const int arrayFoodSize = 10;
+    const int arrayFoodSize = 18;
     std::string foods[arrayFoodSize];
 
-    fill(foods, foods +arrayFoodSize, "Burger");
+
+    fill(foods, foods +(arrayFoodSize/3), "Burger");
+    fill((foods + arrayFoodSize/3), foods + (arrayFoodSize/3)*2, "Chicken");
+    fill(foods + (arrayFoodSize/3)*2, foods + arrayFoodSize, "Kebab");
 
     // std::cout << foods[0];
     for(std::string food : foods){
