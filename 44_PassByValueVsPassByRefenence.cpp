298f186ffcab11afe08c36bd903d023aad94ca63
Domm29 <samaritan723@gmail.com>
2023-03-28 09:18:15 +0100

    referencing to memories

--- 44_PassByValueVsPassByRefenence.cpp
+++ 44_PassByValueVsPassByRefenence.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <cmath>
 
+void swapOrder(std::string &A, std::string &B);
 int main() {
 
+    // 
 
+    std::string x = "Kool-Aid";
+    std::string y = "Water";
+    std::string temp; 
 
+    // temp = x;
+    // x = y;
+    // y = temp;
 
-
-
-
+    swap(x,y);
+    std::cout << "X: " << x << '\n';
+    std::cout << "Y: " << y << '\n';
 
     return 0;
+}
+
+// you can reference the current memory of the varialbe to be 
+// switched by using the `&` before the variable so that the 
+// memory is updated 
+void swapOrder(std::string &A, std::string &B){
+
+    std::string temp;
+    temp = A;
+    A = B;
+    B = temp;
+    // temp = x;
+    // x = y;
+    // y = temp;
+
+   
+    
 }
