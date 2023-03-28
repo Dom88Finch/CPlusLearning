d7ce2ec2f14bffdf5d6636d37649f836abcaf055
Domm29 <samaritan723@gmail.com>
2023-03-28 16:59:35 +0100

    constuctor overload

--- 58_ConstructorsOverloading.cpp
+++ 58_ConstructorsOverloading.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 #include <cmath>
 
+
+
+class Burger{
+    public:
+        std::string topping1;
+        std::string topping2;
+
+    // can have multiple constructors with same name
+    // as long as the parameters are DIFFERENT
+    Burger(){
+    }
+    Burger(std::string topping1){
+        this->topping1 = topping1;
+    }
+    Burger(std::string topping1,std::string topping2 ){
+        this->topping1 = topping1;
+        this->topping2 = topping2;
+    }
+
+};
+
 int main() {
+
+    /*
+    Overloaded constructors = allows for multiple constructors
+            with SAME NAME but different parameters
+    */
+
+   Burger burger1("Lettuce", "BBQ sause ");
+
+   Burger burger2;
+   std::cout << burger1.topping1 << '\n';
+   std::cout << burger1.topping2 << '\n';
+//    std::cout << burger2 << '\n';
 
 
 
