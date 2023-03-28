1048754ea8c9be2b9b24cdbef36fcc0b3e2810d2
Domm29 <samaritan723@gmail.com>
2023-03-28 09:09:19 +0100

    memory address

--- 43_MemoryAddresses.cpp
+++ 43_MemoryAddresses.cpp
@@ -7,8 +7,14 @@ int main() {
     // Memory address - location in memory where data is stored
     //                 this can be accessed with `&` (address of operator)
 
-    
+    std::string name = "Bro";
+    int age = 21;
+    bool student = true;
 
+    // diplays location where the variable is stored in form of Hexadecimal
+    std::cout << &name << '\n'; 
+    std::cout << &age << '\n';
+    std::cout << &student << '\n'; // smallest memory since only boolean
 
 
 
