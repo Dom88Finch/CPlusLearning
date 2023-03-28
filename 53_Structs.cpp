73595ae598096e0b8d4c3c2c0f69ed3db21a730b
Domm29 <samaritan723@gmail.com>
2023-03-28 15:58:15 +0100

    structs

--- 53_Structs.cpp
+++ 53_Structs.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <cmath>
 
+struct cars{
+    std::string model;
+    double cost;
+    bool available;
+};
+
+
 int main() {
 
+    /*
+    Struct = structure that groups related variables under the SAME name.
+        can store multiple values of different datatypes (string, int, ...)
+
+        variables = members
+
+        can be accesd with `.` Class Member Access Operation
+
+    */
 
 
+
+   cars car1;
+   car1.cost = 42000;
+   car1.model = "Nissan GTR";
+   car1.available = false;
+
+   std::cout << car1.model << '\n';
+   std::cout << car1.cost << '\n';
+   std::cout << car1.available << '\n';
 
 
     return 0;
--- 54_PassStructsAsArguments.cpp
+++ 54_PassStructsAsArguments.cpp
@@ -1,11 +1,67 @@
 #include <iostream>
 #include <cmath>
 
+struct Food{
+    std::string foodType;
+    double calories;
+    double cost;
+    bool available = true;
+    double discount;
+
+};
+
+void displayData(Food &snacks);
+
+void applyPriceCoupon(Food &snacks, double discount);
 int main() {
 
+    Food food1;
+    Food food2;
+
+    food1.foodType = "Burger";
+    food1.calories = 550;
+    food1.cost = 4.99;
+
+    food2.foodType = "Chicken Breast";
+    food2.calories = 650;
+    food2.cost = 6.99;
+
+    applyPriceCoupon(food1, 0.70);
+
+    displayData(food1);
+    // displayData(food2);
+    // std::cout << food1.foodType << '\n';
+    // std::cout << food1.calories << '\n';
+    // std::cout << food1.cost << '\n';
+    // std::cout << food1.available << '\n';
+
+    // std::cout << food2.foodType << '\n';
+    // std::cout << food2.calories << '\n';
+    // std::cout << food2.cost << '\n';
+    // std::cout << food2.available << '\n';
+    // std::cout << &food1 << '\n';
 
 
 
 
     return 0;
+}
+
+void displayData(Food &snacks){
+
+    // std::cout << &snacks << '\n';
+    std::cout << snacks.foodType << '\n';
+    std::cout << snacks.calories << '\n';
+    std::cout << snacks.cost << '\n';
+    std::cout << snacks.available << '\n';
+    std::cout << snacks.discount << '\n';
+
+}
+
+void applyPriceCoupon(Food &snacks, double discount){
+
+    snacks.discount = snacks.cost * discount;
+
+    // std::cout << snacks.discount << '\n';
+    
 }
