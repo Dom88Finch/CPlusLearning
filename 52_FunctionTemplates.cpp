afffe127dde2157c85b9f2c4efa50de2d7634b9c
Domm29 <samaritan723@gmail.com>
2023-03-28 15:29:49 +0100

    using different types of function templates

--- 52_FunctionTemplates.cpp
+++ 52_FunctionTemplates.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <cmath>
 
+template <typename T, typename U> // template parameter declaration
+/*
+// instead of repeating the same function with differnt inputs
+int max(int x, int y){
+    return (x > y) ? x : y;
+}
+double max(double x, double y){
+    return (x > y) ? x : y;
+}
+*/
+
+// replace our type with the template parameter `T`
+
+// T max(T x, U y){
+//     return (x > y) ? x : y;
+// }
+
+// using `auto` instead of `T` or `U`, the  compiler returns the correct
+// type when we run teh function 
+auto max(T x, U y){
+    return (x > y) ? x : y;
+}
 int main() {
 
 
+    // function template - describes what function looks like.
+    // uses: used to generate as many overloaded functions as requried each using differnt data types
+
+
+    // 
+    std::cout << max(1.2,2.4) << '\n';
+
+    // by modifying our template to recieve up to 2 different datatypes
+    std::cout << max(1,2.333) << '\n';
 
 
 
