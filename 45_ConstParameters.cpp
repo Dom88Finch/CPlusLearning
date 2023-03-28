97868f4d99a35c18cef6b077b178076500c0c8f5
Domm29 <samaritan723@gmail.com>
2023-03-28 09:25:30 +0100

    using const parameters

--- 45_ConstParameters.cpp
+++ 45_ConstParameters.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <cmath>
 
+
+
+void printInfo(const std::string name, const int age);
 int main() {
 
 
 
+    /* const parameter = parameter that read-only 
 
+    advantages: code is more secure & conveys the intent
+                it is also useful for references & pointers
 
+    */
+
+    std::string name = "Micheal";
+    int age = 23;
+
+    printInfo(name, age);
 
 
 
     return 0;
+}
+
+// by using `const`, we ensure that we cannot modify the variables
+void printInfo(const std::string name, const int age){
+
+    name = "wono";
+    age = 193;
+
+    std::cout << name <<'\n';
+    std::cout << age << '\n';
+
 }
