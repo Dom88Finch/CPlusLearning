64708140a26d25f53593b1124f8989a4ea6daed7
Domm29 <samaritan723@gmail.com>
2023-06-06 12:14:23 +0100

    heart

---
+++ 62_heart.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#define C(a) ((a)*(a)*(a))
+int main(){
+    
+    int x,y;
+
+    for(y=9;y>-6;y--)
+    {
+        for(x=-8;x<9;x++)
+        {
+            putchar(C (x*x+y*y-25)<25*x*x*y*y*y ? "*##*"[(x+10)%4] : '-');
+        }
+
+        putchar('\n');
+    }
+
+    return 0;
+}
